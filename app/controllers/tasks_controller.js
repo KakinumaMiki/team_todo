0999f3cf8b5ad315ac58c3f2f6406e1525c1f41a
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-18 19:47:09 +0900

    タスクの担当者の作成更新

--- app/controllers/tasks_controller.js
+++ app/controllers/tasks_controller.js
@@ -12,13 +12,26 @@ class ExamplesController extends Controller {
 
   // POST /
   async store(req, res) {
+    const user = await this._user(req);
     const team = await this._team(req);
-    const task = models.Task.build({
-      teamId: team.id,
-      title: req.body.title,
-      body: req.body.body,
-      creatorId: req.user.id
-    });
+    let task = "";
+    if (user){
+      task = models.Task.build({
+        teamId: team.id,
+        title: req.body.title,
+        body: req.body.body,
+        creatorId: req.user.id,
+        assigneeId: user.id
+      });
+    } else {
+      task = models.Task.build({
+        teamId: team.id,
+        title: req.body.title,
+        body: req.body.body,
+        creatorId: req.user.id
+      });
+    }
+
     await task.save();
     await req.flash('info', `タスク[${task.title}]を保存しました`);
     res.redirect(`/teams/${team.id}`);
@@ -27,16 +40,34 @@ class ExamplesController extends Controller {
   // GET /:id/edit
   async edit(req, res) {
     const task = await this._task(req);
-    res.render('tasks/edit', { task });
+    const team = await this._team(req);
+    const memberUsers = await team.getMemberUsers();
+    res.render('tasks/edit', { task, memberUsers });
   }
 
   // PUT or PATCH /:id
   async update(req, res) {
-    const task = await this._task(req);
-    task.set(req.body);
+    const user = await this._user(req);
+    const team = await this._team(req);
+    let task = await this._task(req);
+    if (user){
+      task.set({
+        teamId: team.id,
+        title: req.body.title,
+        body: req.body.body,
+        creatorId: req.user.id,
+        assigneeId: user.id
+      });
+    } else {
+      task.set({
+        teamId: team.id,
+        title: req.body.title,
+        body: req.body.body,
+        creatorId: req.user.id
+      });
+    }
     await task.save();
     await req.flash('info', `[${task.title}]を更新しました`);
-
     res.redirect(`/teams/${task.teamId}`);
   }
 
@@ -54,6 +85,18 @@ class ExamplesController extends Controller {
       throw new Error('Task not find');
     }
     return task;
+  }
+
+  async _user(req) {
+    if(req.body.displayName) {
+      const user = await models.User.findOne({ where : { displayName: req.body.displayName } });
+      if (!user) {
+        throw new Error('Task not find');
+      }
+      return user;
+    } else {
+      return null;
+    }
   }
 
 }
