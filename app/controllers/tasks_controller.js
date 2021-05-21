2bf637b0026c7107f350dc2fcda53c28287ca381
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-21 17:18:37 +0900

    レビュー指摘反映

--- app/controllers/tasks_controller.js
+++ app/controllers/tasks_controller.js
@@ -12,16 +12,15 @@ class ExamplesController extends Controller {
 
   // POST /
   async store(req, res) {
-    const user = await this._user(req);
     const team = await this._team(req);
     let task = "";
-    if (user){
+    if (req.body.id){
       task = models.Task.build({
         teamId: team.id,
         title: req.body.title,
         body: req.body.body,
         creatorId: req.user.id,
-        assigneeId: user.id
+        assigneeId: req.body.id
       });
     } else {
       task = models.Task.build({
@@ -47,16 +46,15 @@ class ExamplesController extends Controller {
 
   // PUT or PATCH /:id
   async update(req, res) {
-    const user = await this._user(req);
     const team = await this._team(req);
     let task = await this._task(req);
-    if (user){
+    if (req.body.id){
       task.set({
         teamId: team.id,
         title: req.body.title,
         body: req.body.body,
         creatorId: req.user.id,
-        assigneeId: user.id
+        assigneeId: req.body.id
       });
     } else {
       task.set({
@@ -86,19 +84,6 @@ class ExamplesController extends Controller {
     }
     return task;
   }
-
-  async _user(req) {
-    if(req.body.displayName) {
-      const user = await models.User.findOne({ where : { displayName: req.body.displayName } });
-      if (!user) {
-        throw new Error('Task not find');
-      }
-      return user;
-    } else {
-      return null;
-    }
-  }
-
 }
 
 
