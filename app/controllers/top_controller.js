37e3d0c21574490b28a4c16beea9e525be34c186
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-05-24 12:29:32 +0900

    レビュー指摘対応（titleをviewに直書き）

--- app/controllers/top_controller.js
+++ app/controllers/top_controller.js
@@ -17,9 +17,9 @@ class TopController extends Controller {
         order: [['teamId', 'ASC']],
         where: { assigneeId: user.id }
       });
-      return res.render('index', { title: 'Express', user, tasks, members });
+      return res.render('index', { user, tasks, members });
     }
-    return res.render('index', { title: 'Express' });
+    return res.render('index');
   }
 
 }
